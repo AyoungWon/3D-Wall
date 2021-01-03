4324dbf29697dd8a8473c37cac9f77e0d1856849
AyoungWon <thwn4750@gmail.com>
2021-01-03 17:41:14 +0900

    scroll bar 완성

--- js/wall3d.js
+++ js/wall3d.js
@@ -1,6 +1,7 @@
 (function() {
 
     const houseElem = document.querySelector('.house');
+    const barElem = document.querySelector('.progress-bar')
     let maxScrollValue;
     
     function resizeHandler() {
@@ -10,8 +11,13 @@
 
     window.addEventListener('scroll', function() {
     // pageYOffset/maxScrollValue = 전체 스크롤 범위에서 현재 스크롤된 비율
-        const zMove = pageYOffset/maxScrollValue * 980 - 490;
+        const scrollPer = pageYOffset/maxScrollValue;
+        const zMove =  scrollPer * 980 - 490; //translateZ의 값
         houseElem.style.transform = 'translateZ('+ zMove +'vw)'
+
+        //progress bar
+        barElem.style.width =  scrollPer * 100 + '%'
+
     });
 
     window.addEventListener('resize', resizeHandler);
