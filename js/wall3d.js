d32e310c2fa0f853c6172932f3fcaa114a98cbfc
AyoungWon <thwn4750@gmail.com>
2021-01-03 18:21:19 +0900

    mouse move 완성

--- js/wall3d.js
+++ js/wall3d.js
@@ -1,7 +1,8 @@
 (function() {
-
+    const stageElem = document.querySelector('.stage');
     const houseElem = document.querySelector('.house');
     const barElem = document.querySelector('.progress-bar')
+    const mousePos = { x: 0, y: 0};
     let maxScrollValue;
     
     function resizeHandler() {
@@ -19,6 +20,18 @@
         barElem.style.width =  scrollPer * 100 + '%'
 
     });
+
+    window.addEventListener('mousemove', function( e ) {
+       // e.clientX,e.clientY mouse의 위치 좌표
+       // e.clientX / window.innerWidth 화면의 x축에서 마우스의 이동 비율
+       mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+       mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+       stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) +'deg) rotateY(' + (mousePos.x * 5) +'deg)'
+
+    })
+
+
+
 
     window.addEventListener('resize', resizeHandler);
     resizeHandler(); //load되면 한번 초기화
